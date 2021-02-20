f6f0d1c92534addb6f9cbea936f8cebf1665fe68
Catherine Gloria <catherinegloria@gmail.com>
2021-02-20 15:51:42 -0800

    jobs controller index, create, strong params

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -1,2 +1,28 @@
 class Api::V1::JobsController < ApplicationController
+
+  def index
+    jobs = Job.all
+    render json: JobSerializer.new(jobs)
+  end
+
+  def create
+    job = Job.new(job_params)
+    if job.save
+      render json: job, status: :accepted
+    else
+      render json: {errors: job.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(
+      :title,
+      :image_url,
+      :description,
+      :helper,
+      :chart_id)
+  end
+
 end
