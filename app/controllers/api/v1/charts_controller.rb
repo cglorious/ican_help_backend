4ce848d8280961caa7f794dcfeed5bf784a7bb45
Catherine Gloria <catherinegloria@gmail.com>
2021-02-20 15:01:31 -0800

    charts create action and strong params

--- app/controllers/api/v1/charts_controller.rb
+++ app/controllers/api/v1/charts_controller.rb
@@ -5,4 +5,19 @@ class Api::V1::ChartsController < ApplicationController
     render json: ChartSerializer.new(charts)
   end
 
+  def create
+    chart = Chart.new(chart_params)
+    if chart.save
+      render json: chart, status: :accepted
+    else
+      render json: {errors: chart.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def chart_params
+    params.require(:chart).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :charts, only: [:index]
+      resources :charts, only: [:index, :create]
     end
   end
 end
