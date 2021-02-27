f6d23dd5a75386ebb42e187805982ae62b567cf7
Catherine Gloria <catherinegloria@gmail.com>
2021-02-27 12:26:32 -0800

    adjusted seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,42 +9,35 @@
 Chart.delete_all
 Job.delete_all
 
-kinder = Chart.create(name: "Kindergarten")
-girl_scout = Chart.create(name: "Girl Scout")
-chore = Chart.create(name: "Chore")
+classroom = Chart.create(name: "Classroom")
+home = Chart.create(name: "Home")
 
-#Kindergarten Chart
+#Classroom Chart
 librarian = Job.create(
   title: "Librarian",
   image_url: "https://content.mycutegraphics.com/graphics/school/boy-classroom-librarian-clipart-jobs-classroom-helper-clip-art.png",
   description: "I can choose a book to read.",
   helper: "Maren",
-  chart_id: kinder.id)
+  chart_id: classroom.id)
 door_holder = Job.create(
   title: "Door Holder",
   image_url: "https://i.pinimg.com/originals/24/80/4a/24804a461a1f0ba9601a0191e3ace671.png",
   description: "I can hold the door for others.",
   helper: "Jesse",
-  chart_id: kinder.id)
+  chart_id: classroom.id)
 line_leader = Job.create(
   title: "Line Leader",
   image_url: "https://content.mycutegraphics.com/graphics/school/line-counter-clip-art-classroom-jobs-clip-art.png",
   description: "I can lead the line.",
   helper: "Ralph",
-  chart_id: kinder.id)
-
-#Girl Scout Chart
+  chart_id: classroom.id)
 distribution  = Job.create(
   title: "Distributor",
   image_url: "https://content.mycutegraphics.com/graphics/school/boy-supply-monitor-clipart-classroom-job-clip-art.png",
   description: "I can pass out supplies.",
   helper: "Sadie",
-  chart_id: girl_scout.id)
-bonatist = Job.create(title: "Botanist",
-  image_url: "https://lh3.googleusercontent.com/proxy/n4k_6svZ3vqGdjAbuVkMe6Cgs4ynT6ygurq4_y8WMO5AvinU34kb7YZfXskkdM7qRcce7R5N9v30rJOqS5nkr_a2MgINQuPQ66YzGYa3ndNqBg",
-  description: "I can water the plants.",
-  helper: "Abigail",
-  chart_id: girl_scout.id)
+  chart_id: classroom.id)
+
 
 #Chore Chart
 veterinarian = Job.create(
@@ -52,10 +45,10 @@ veterinarian = Job.create(
   image_url: "https://clipartart.com/images/attendance-helper-clipart-line-leader-9.png",
   description: "I can feed the pet.",
   helper: "Julia",
-  chart_id: chore.id)
+  chart_id: home.id)
 sanitation = Job.create(
   title: "Sanitation",
   image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrkZNnrvprqWe9VRpm_hIo_uUlWcvzsKAq-A&usqp=CAU",
   description: "I can take out the trash.",
   helper: "Annabelle",
-  chart_id: chore.id)
+  chart_id: home.id)
